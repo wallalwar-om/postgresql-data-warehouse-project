f3b6de2c28e36baa63259d5f71157773404b4b3f
omwallalwar-dev <githubdevelopom0196@gmail.com>
2025-08-13 01:39:21 +0530

    run file

---
+++ scripts/setup_all.sql
@@ -0,0 +1,10 @@
+
+\i scripts/create_schema.sql
+
+\i scripts/bronze/proc_load_bronze.sql
+\i scripts/bronze/ddl_bronze.sql
+
+\i scripts/silver/proc_load_silver.sql
+\i scripts/silver/ddl_silver.sql
+
+\i scripts/gold/ddl_gold.sql
